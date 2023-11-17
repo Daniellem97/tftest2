b734ee2d8f387edadb723457cf4a8370060d80fa
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-17 17:37:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
-bucket_key_enabled
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
-bucket_key_enabled = true
+  bucket_key_enabled = true
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
-
   #provisioner "local-exec" {
   #  command = templatefile("${var.host_os}-ssh-config.tpl", {
   #    hostname = self.public_ip,
