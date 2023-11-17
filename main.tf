e62ee1bb070833b924c4dfd60bece073814df369
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-17 17:41:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
-  bucket_key_enabled = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "your-kms-key-id"
+      }
+    }
+  }
 
   tags = {
     Name        = "My bucket"
