5eb6341fb620272937001d46a943690cfef0c507
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-17 18:06:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,7 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "your-kms-key-id"
-      }
-    }
-  }
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
   #provisioner "local-exec" {
   #  command = templatefile("${var.host_os}-ssh-config.tpl", {
   #    hostname = self.public_ip,
